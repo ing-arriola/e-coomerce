c5f7bab85fd9cb93d0d936c2b12ce4eee2c5896e
ing-arriola <ingeniero.arriola@gmail.com>
2020-05-09 14:57:59 -0600

    Added styles and motion to second carrousel

--- js/main.js
+++ js/main.js
@@ -9,3 +9,14 @@ $('.slider-one')
     prevArrow:".site-slider .slider-btn .prev",
     nextArrow:".site-slider .slider-btn .next"
   });
+
+  $('.slider-two')
+  .not(".slick-intialized")
+  .slick({
+      prevArrow:".site-slider-two .prev",
+      nextArrow:".site-slider-two .next",
+      slidesToShow:4,
+      slidesToScroll:1,
+      autoplaySpeed:900
+
+    });
